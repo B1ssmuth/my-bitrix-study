802e7d3360105a238e6cf756f88d2fc667b17ef7
Илья Богданов <ilybogdanow@yandex.ru>
2026-05-15 13:50:30 +0300

    Fix id table

--- local/App/Models/Lists/ProceduresTable.php
+++ local/App/Models/Lists/ProceduresTable.php
@@ -3,5 +3,5 @@ namespace App\Models\Lists;
 use App\Models\AbstractIblockPropertyValuesTable;
 
 class ProceduresTable extends AbstractIblockPropertyValuesTable {
-    public const IBLOCK_ID = 16; //в локалке 16, на проде 18
+    public const IBLOCK_ID = 18; //в локалке 16, на проде 18
 }
