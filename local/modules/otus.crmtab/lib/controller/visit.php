91651a2ee8f0f7f0c93168f7cbd3625250fea735
Илья Богданов <ilybogdanow@yandex.ru>
2026-05-15 16:19:23 +0300

    fix homework6 visit.php controller

--- local/modules/otus.crmtab/lib/controller/visit.php
+++ local/modules/otus.crmtab/lib/controller/visit.php
@@ -3,9 +3,22 @@ namespace Otus\CrmTab\Controller;
 
 use Bitrix\Main\Engine\Controller;
 use App\Models\VisitLogTable;
+// Добавь этот импорт для фильтров
+use Bitrix\Main\Engine\ActionFilter;
 
 class Visit extends Controller
 {
+    public function configureActions()
+    {
+        return [
+            'getCount' => [
+                'prefilters' => [
+                    new ActionFilter\Csrf(false),
+                ],
+            ],
+        ];
+    }
+
     public function getCountAction()
     {
         return [
