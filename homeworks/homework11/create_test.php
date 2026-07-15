3bfcbd239b67df070df5f40748a4704a026a64c1
Илья Богданов <ilybogdanow@yandex.ru>
2026-07-15 11:40:41 +0300

    new file

---
+++ homeworks/homework11/create_test.php
@@ -0,0 +1,32 @@
+<?php
+$webhookUrl = 'http://ce255660.tw1.ru/rest/1/vw65vprt35ii6b2h/crm.activity.add.json';
+
+$contactId = 15; 
+
+$queryData = http_build_query([
+    'fields' => [
+        'OWNER_TYPE_ID' => 3,
+        'OWNER_ID' => $contactId,
+        'TYPE_ID' => 2,
+        'SUBJECT' => 'Тестовое дело из REST API (Хак)',
+        'DIRECTION' => 2,
+        'COMPLETED' => 'N'
+    ]
+]);
+
+$curl = curl_init();
+curl_setopt_array($curl, [
+    CURLOPT_POST => 1,
+    CURLOPT_HEADER => 0,
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $webhookUrl,
+    CURLOPT_POSTFIELDS => $queryData,
+]);
+
+$result = curl_exec($curl);
+curl_close($curl);
+
+echo "<h1>Команда отправлена!</h1>";
+echo "<p>Ответ Битрикса на создание дела:</p>";
+echo "<pre>" . print_r(json_decode($result, true), true) . "</pre>";
+echo "<p>Если выше написано result => ID, значит дело успешно создано!<br>Теперь иди проверяй webhook_log.txt и карточку Контакта!</p>";
