b3ffed41e2a66ba67cf5d22d484a5cf82664d17d
B1ssmuth <ilybogdanow@yandex.ru>
2026-08-04 09:27:19 +0000

    fix events

--- local/modules/otus.crmtab/lib/events.php
+++ local/modules/otus.crmtab/lib/events.php
@@ -11,22 +11,23 @@ Loc::loadMessages(__FILE__);
 class Events
 {
     /**
-     * Обработчик добавления кастомной вкладки в карточку CRM
+     * Обработчик добавления кастомной вкладки в карточку CRM.
+     * Название метода оставлено addVisitLogTab для совместимости со старой регистрацией события.
      */
-    public static function onEntityDetailsTabsInitialized(Event $event)
+    public static function addVisitLogTab(Event $event)
     {
         $entityID = $event->getParameter('entityID');
         $entityTypeID = $event->getParameter('entityTypeID');
         $tabs = $event->getParameter('tabs');
 
-        // По ТЗ: вкладка должна быть у контакта-физлица[cite: 2]
+        // По ТЗ: вкладка должна быть у контакта-физлица
         if ($entityTypeID === \CCrmOwnerType::Contact) {
             $tabs[] = [
                 'id' => 'garage_tab',
-                // Используем языковую фразу, если нет - fallback на "Гараж"[cite: 2]
+                // Используем языковую фразу, если нет - fallback на "Гараж"
                 'name' => Loc::getMessage('OTUS_CRMTAB_GARAGE_NAME') ?: 'Гараж',
                 'loader' => [
-                    // Ссылка на lazyload файл нашего будущего компонента
+                    // Ссылка на lazyload файл нашего компонента
                     'serviceUrl' => '/local/components/otus/cars.grid/lazyload.php?&site=' . SITE_ID . '&' . bitrix_sessid_get(),
                     'componentData' => [
                         'template' => '',
